1a5716e9bf6bdc4bbe97726737f5decc9991501a
ljbmoura <ljbmoura@gmail.com>
2017-05-30 07:45:02 -0300

    fim aula 3

---
+++ www/js/controller.js
@@ -0,0 +1,27 @@
+/**
+ * criado em 30/05/2017 
+ */
+
+angular.module('starter')
+.controller(
+	'ListagemController',
+	function ($scope) {
+	    
+	    $scope.listaDeCarros = [
+		'BMW 120i',
+		'ONIX 1.6',
+		'Fiesta 2.0',
+		'C3 1.0',
+		'Uno Fire',
+		'Sentra 2.0',
+		'Astra Sedan',
+		'Vectra 2.0',
+		'Hilux 4x4',
+		'Montana Cabine dupla',	
+		'Outlander 2.4',  
+		'Fusca 1500'];
+	    
+	    
+	
+	}
+)
