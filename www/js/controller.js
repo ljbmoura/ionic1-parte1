d1718cdccedc777b45e88f4bbb76c93d08f23833
ljbmoura <ljbmoura@gmail.com>
2017-06-02 00:05:00 -0300

    fim aula 4

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,29 +1,19 @@
 /**
- * criado em 30/05/2017 
+ * criado em 30/05/2017
  */
 
 var moduloPrincipal = angular.module('starter');
 
-moduloPrincipal.controller(
-'ListagemController',
-function ($scope) {
-    	
-    	$scope.listaDeCarros = [
-		'BMW 120i',
-		'ONIX 1.6',
-		'Fiesta 2.0',
-		'C3 1.0',
-		'Uno Fire',
-		'Sentra 2.0',
-		'Astra Sedan',
-		'Vectra 2.0',
-		'Hilux 4x4',
-		'Montana Cabine dupla',	
-		'Outlander 2.4',  
-		'Fusca 1500'];
-    	$scope.data = '01/10/2016';
-}
-	
-	
+moduloPrincipal.controller('ListagemController', function($scope) {
 
-)
+    $scope.listaDeCarros = [ "BMW 120i", "Onix 1.6", "Fiesta 2.0", "C3 1.0", "Uno Fire", "Sentra 2.0", "Astra Sedan",
+	    "Vectra 2.0 Turbo", "Hilux 4x4", "Montana Cabine dupla", "Outlander 2.4", "Fusca 1500" ];
+
+});
+
+moduloPrincipal.controller('CarroEscolhidoController', function($scope, $stateParams) {
+    
+    $scope.carroEscolhido = $stateParams.carro;
+
+});
+
