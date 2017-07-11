92f378aa490a4c88d392c8ebb63a75c87eae2e9d
ljbmoura <ljbmoura@gmail.com>
2017-07-11 09:06:22 -0300

    fim aula 7

--- www/js/controller.js
+++ www/js/controller.js
@@ -8,56 +8,45 @@ moduloPrincipal.controller('ListagemController', function($scope, CarroService) 
 
     CarroService.obterCarros().then(function(carros) {
 	$scope.listaDeCarros = carros;
-//    }, function(reason) {
-//    	
-//    }, function(value) {
-//    	
+	// }, function(reason) {
+	//    	
+	// }, function(value) {
+	//    	
     });
-/*    $scope.listaDeCarros = [ {
-	nome : "BMW 120i",
-	preco : 100000.00
-    }, {
-	nome : "Onix 1.6",
-	preco : 50000
-    }, {
-	nome : "Uno Fire",
-	preco : 30000
-    }, {
-	nome : "Sentra 2.0",
-	preco : 82000
-    }, {
-	nome : "Fusca 1500",
-	preco : 973000
-    }
-    ,{nome : "Outlander 2.4", preco : 99000}
-    ];
-*/    
-/*    [{"nome" : "BMW 120i" , "preco" : 70000},
-        {"nome" : "Onix 1.6" , "preco" : 35000},
-        {"nome" : "Fiesta 2.0", "preco" : 52000},
-        {"nome" : "C3 1.0", "preco" : 22000},
-        {"nome" : "Uno Fire", "preco" : 11000},
-        {"nome" : "Sentra 2.0", "preco" : 53000},
-        {"nome" : "Astra Sedan", "preco" : 39000},
-        {"nome" : "Vectra 2.0", "preco" : 37000},
-        {"nome" : "Hilux 4x4", "preco" : 90000},
-        {"nome" : "Montana Cabine dupla", "preco" : 57000},
-        {"nome" : "Outlander 2.4", "preco" : 99000},
-        {"nome" : "Fusca 15000", "preco" : 6000}
-      ];*/    
+    /*
+     * $scope.listaDeCarros = [ { nome : "BMW 120i", preco : 100000.00 }, { nome :
+     * "Onix 1.6", preco : 50000 }, { nome : "Uno Fire", preco : 30000 }, { nome :
+     * "Sentra 2.0", preco : 82000 }, { nome : "Fusca 1500", preco : 973000 }
+     * ,{nome : "Outlander 2.4", preco : 99000} ];
+     */
+    /*
+     * [{"nome" : "BMW 120i" , "preco" : 70000}, {"nome" : "Onix 1.6" , "preco" :
+     * 35000}, {"nome" : "Fiesta 2.0", "preco" : 52000}, {"nome" : "C3 1.0",
+     * "preco" : 22000}, {"nome" : "Uno Fire", "preco" : 11000}, {"nome" :
+     * "Sentra 2.0", "preco" : 53000}, {"nome" : "Astra Sedan", "preco" :
+     * 39000}, {"nome" : "Vectra 2.0", "preco" : 37000}, {"nome" : "Hilux 4x4",
+     * "preco" : 90000}, {"nome" : "Montana Cabine dupla", "preco" : 57000},
+     * {"nome" : "Outlander 2.4", "preco" : 99000}, {"nome" : "Fusca 15000",
+     * "preco" : 6000} ];
+     */
 
 });
 
 moduloPrincipal.controller('CarroEscolhidoController', function($scope, $stateParams) {
 
     $scope.carroEscolhido = angular.fromJson($stateParams.carro);
-    
-    $scope.listaDeAcessorios = [
-	{nome: "Ar Condicionado", preco: 4000.50},
-	{nome: "Freio ABS", preco: 6538.79},
-	{nome: "Banco de Couro", preco: 9470.23}
-    ];
-    
+
+    $scope.listaDeAcessorios = [ {
+	nome : "Ar Condicionado",
+	preco : 4000.50
+    }, {
+	nome : "Freio ABS",
+	preco : 6538.79
+    }, {
+	nome : "Banco de Couro",
+	preco : 9470.23
+    } ];
+
     $scope.opcaoAcessorioClicado = function(acessorio, isMarcado) {
 	if (isMarcado) {
 	    $scope.carroEscolhido.preco += acessorio.preco;
@@ -68,43 +57,43 @@ moduloPrincipal.controller('CarroEscolhidoController', function($scope, $statePa
 
 });
 
+moduloPrincipal.controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state,
+	CarroService) {
 
-moduloPrincipal.controller('FinalizarPedidoController', function($stateParams, $scope, $ionicPopup, $state, CarroService) {
-    
     $scope.carroFinalizado = angular.fromJson($stateParams.carro);
-    
-    $scope.pedido = {};
-    
+
+    $scope.pedido = { nome: 'luciano'
+	,endereco: 'endereço', email:'email@email'};
+
     $scope.finalizarPedido = function() {
-	
+
 	var pedidoCompleto = {
-	params: {
-        	    carro: $scope.carroFinalizado.nome,
-            	    preco: $scope.carroFinalizado.preco,
-            	    nome: $scope.pedido.nome,
-            	    endereco: $scope.pedido.endereco,
-            	    email: $scope.pedido.email
-        	}
+	    params : {
+		carro : $scope.carroFinalizado.nome,
+		preco : $scope.carroFinalizado.preco,
+		nome : $scope.pedido.nome,
+		endereco : $scope.pedido.endereco,
+		email : $scope.pedido.email
+	    }
 	};
-	
-	CarroService.salvarPedido(pedidoCompleto)
-	.then(function(dados) {
+
+	CarroService.salvarPedido(pedidoCompleto).then(
+	   function(dados) {
 	    console.debug(dados);
 	    $ionicPopup.alert({
-		title: 'Parabéns', // String. The title of the popup.
-		template: 'Você acaba de comprar um carro',
-		okText: 'Sonho Realizado, modelo:'  // String (default: 'OK'). The text of the OK button.
+		title : 'Parabéns', // String. The title of the popup.
+		template : 'Você acaba de comprar um ' + dados,
+		okText : 'Sonho Realizado' // String (default: 'OK').
 	    }).then(function() {
 		$state.go('listagem');
 	    });
-	})
-	,function(erro){
-	    console.log(erro);
-	    	    $ionicPopup.alert({
-	    	      title: 'Deu erro',
-	    	      template: 'Campos obrigatórios'
-	    	      })
+	}, function(erro) {
+	    console.debug(erro);
+	    $ionicPopup.alert({
+		title : 'Deu erro',
+		template : 'Campos obrigatórios'
+	    })
 	}
-	
+	)
     }
 })
--- www/js/service.js
+++ www/js/service.js
@@ -1,37 +1,40 @@
 /**
- * serviços padrão AngularJS 
+ * serviços padrão AngularJS
  */
 var moduloPrincipal = angular.module('starter');
 
 moduloPrincipal.service('CarroService', function($http) {
-    
+
     var url = 'http://aluracar.herokuapp.com/';
-    
+
     return {
-	obterCarros: function () {
-	  return $http.get(url).then(
-	    function(listaCarros) {
-		//console.debug(listaCarros.data);
-	    	return listaCarros.data;
-	    }
-	    , function(reason) {
-	    	console.log('Erro GET ' + url + ' : ' + reason.statusText);
-	    }
-	  )
+	obterCarros : function() {
+	    return $http.get(url).then(function(listaCarros) {
+		// console.debug(listaCarros.data);
+		return listaCarros.data;
+	    }, function(reason) {
+		console.log('Erro GET ' + url + ' : ' + reason.statusText);
+	    })
 	}
-    
-    	,salvarPedido: function(pedido) {
-	  return $http.get(url + 'salvarpedido', pedido).then(
-	    function(reposta) {
-		console.log('pedido acolhido com sucesso:' + repostaOk);
-		return reposta;
+
+	,
+	salvarPedido : function(pedido) {
+	    return $http.get(url + 'salvarpedido', pedido).then(function(sucesso) {
+		console.debug('status resposta: ' + sucesso.status);
+		console.debug(sucesso.data);
+		console.log('pedido acolhido com sucesso!');
+		return sucesso.data.carro.nome;
 	    }
-	  )
-	  
+//	    , function(falha) {
+//		console.debug(falha);
+//		console.debug('status resposta: ' + falha.status);
+//		console.debug(falha.data);
+//		console.log('Erro GET '+ url + 'salvarpedido');
+//		return 'falhou';
+//	    }
+	    )
 
 	}
     }
-    
-    
-	
+
 })
